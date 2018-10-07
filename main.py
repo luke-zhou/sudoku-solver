073291eed55bb4690d14f510331f0d5ea874bbbf
Luke Zhou <sahrlock@gmail.com>
2018-10-07 22:12:15 +1100

    add easy problem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-problem = [
+problem_hard = [
     [0, 0, 0, 0, 1, 7, 5, 0, 0],
     [0, 0, 0, 0, 0, 2, 0, 0, 0],
     [7, 4, 8, 0, 0, 0, 2, 0, 0],
@@ -10,9 +10,21 @@ problem = [
     [0, 6, 0, 0, 0, 9, 0, 0, 8]
 ]
 
+problem_easy = [
+    [7, 0, 0, 3, 0, 9, 0, 0, 6],
+    [0, 2, 6, 0, 0, 0, 0, 1, 8],
+    [0, 0, 0, 0, 2, 6, 7, 0, 0],
+    [4, 8, 0, 0, 0, 0, 5, 0, 3],
+    [0, 0, 2, 8, 9, 0, 0, 0, 0],
+    [0, 9, 0, 7, 0, 3, 0, 8, 0],
+    [0, 0, 9, 4, 0, 2, 3, 0, 0],
+    [0, 0, 3, 9, 0, 0, 0, 5, 4],
+    [1, 5, 0, 0, 0, 8, 2, 0, 0]
+]
+
 
 def main():
-    sudoku = preprocessing(problem)
+    sudoku = preprocessing(problem_easy)
     updated = True
     while updated:
         updated = False
@@ -24,10 +36,13 @@ def main():
                     updated = updated or changed
 
             sudoku = pivot(sudoku)
-        
+
         print_sudoku(sudoku)
         print(updated)
         print()
+
+        if not updated:
+            break
 
         sudoku = get_square(sudoku)
         for i in range(9):
@@ -37,19 +52,22 @@ def main():
                 updated = updated or changed
 
         sudoku = get_square(sudoku)
-        
+
         print_sudoku(sudoku)
         print(updated)
 
+
 def get_square(sudoku):
-    new_sudoku = [[0]*9 for i in range(9)] 
+    new_sudoku = [[0]*9 for i in range(9)]
     for i in range(9):
         for j in range(9):
             # print(i,j,int((i%3)/3)+int(j/3)+int(i/3)*3, (j%3+i*3)%9)
-            new_sudoku[int((i%3)/3)+int(j/3)+int(i/3)*3][(j%3+i*3)%9] = sudoku[i][j]
+            new_sudoku[int((i % 3)/3)+int(j/3)+int(i/3) *
+                       3][(j % 3+i*3) % 9] = sudoku[i][j]
             # print_sudoku(new_sudoku)
 
     return new_sudoku
+
 
 def pivot(sudoku):
     new_sudoku = [[0]*9 for i in range(9)]
@@ -61,28 +79,31 @@ def pivot(sudoku):
 
     return new_sudoku
 
+
 def preprocessing(sudoku_problem):
     sudoku = sudoku_problem[:]
     for i in range(9):
         for j in range(9):
             if sudoku[i][j] == 0:
-                sudoku[i][j] = [1,2,3,4,5,6,7,8,9]
-    
+                sudoku[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     return sudoku
-    
+
+
 def need_evaluate(group):
     return any([type(e) is list for e in group])
 
+
 def evaluate(group):
     nums = [e for e in group if type(e) is int]
-    num_dict ={}
+    num_dict = {}
     new_group = [0]*9
     for i in range(9):
         if type(group[i]) is list:
-            new_group[i] =[num for num in group[i] if num not in nums]
+            new_group[i] = [num for num in group[i] if num not in nums]
             # only element make it certain
-            if len(new_group[i])==1:
-                new_group[i]=new_group[i][0]
+            if len(new_group[i]) == 1:
+                new_group[i] = new_group[i][0]
                 nums.append(new_group[i])
             else:
                 for e in new_group[i]:
@@ -92,19 +113,22 @@ def evaluate(group):
 
     print(new_group)
 
-    certain_nums =[k for k in num_dict if num_dict[k]==1 and num_dict[k] not in nums]
+    certain_nums = [k for k in num_dict if num_dict[k]
+                    == 1 and num_dict[k] not in nums]
 
     for i in range(9):
         if type(new_group[i]) is list:
             test_result = [num for num in group[i] if num in certain_nums]
-            new_group[i]=test_result[0] if len(test_result)==1 else new_group[i]
-    
-    return new_group, new_group!=group
+            new_group[i] = test_result[0] if len(
+                test_result) == 1 else new_group[i]
+
+    return new_group, new_group != group
 
 
 def print_sudoku(sudoku):
     for row in sudoku:
         print(row)
+
 
 if __name__ == "__main__":
     # result = evaluate([3, 2, [1, 7], [5, 8], 5, 9, 6, 4, [1, 5, 7, 8]])
